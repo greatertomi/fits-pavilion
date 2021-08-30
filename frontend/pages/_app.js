8ca302423caa47f7d85f218fb5b3f561b8ff4816
John Oshalusi <oshalusijohn@gmail.com>
2021-08-30 17:58:18 +0100

    Finished fixing up styles for frontend

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,12 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routerChangeStart', () => NProgress.start());
+Router.events.on('routerChangeComplete', () => NProgress.done());
+Router.events.on('routerChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
