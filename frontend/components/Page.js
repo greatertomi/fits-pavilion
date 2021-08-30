7634cb9b7f11b009496d570b305f18c4e81d3056
John Oshalusi <oshalusijohn@gmail.com>
2021-08-30 17:02:38 +0100

    Added Header and Nav

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,9 @@
+import Header from './Header';
+
 export default function Page({ children }) {
   return (
     <div>
+      <Header />
       <h1>I am a page component</h1>
       {children}
     </div>
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,7 +1,3 @@
 export default function IndexPage() {
-  return (
-    <div>
-      <p>SellPage</p>
-    </div>
-  );
+  return <p>SellPage</p>;
 }
