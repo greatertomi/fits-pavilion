d708892cc3b2185b3c5cafe6f12d5a13ac6284fb
John Oshalusi <oshalusijohn@gmail.com>
2021-08-30 21:53:01 +0100

    Began working on the backend

---
+++ backend/keystone.ts
@@ -0,0 +1,26 @@
+import { createSchema, config } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({}),
+  ui: {
+    isAccessAllowed: () => true,
+  },
+});
