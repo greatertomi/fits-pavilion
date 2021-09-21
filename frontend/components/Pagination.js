4e82d29a9479badc8db3fd6a7d54c430634f86e3
John Oshalusi <oshalusijohn@gmail.com>
2021-09-21 08:32:51 +0100

    Began working on Pagination.js

---
+++ frontend/components/Pagination.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/client';
+import PaginationStyles from './styles/PaginationStyles';
+import DisplayError from './ErrorMessage';
+import { perPage } from '../config';
+
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    _allProductsMeta {
+      count
+    }
+  }
+`;
+
+const Pagination = ({ page }) => {
+  const { error, loading, data } = useQuery(PAGINATION_QUERY);
+  if (loading) return 'Loading...';
+  if (error) return <DisplayError error={error} />;
+  const { count } = data._allProductsMeta;
+  const pageCount = Math.ceil(count / perPage);
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>Fits - Page ${page} of ___</title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        <span aria-disabled={page <= 1}>Prev</span>
+      </Link>
+      <p>
+        Page {page} of ${pageCount}
+      </p>
+      <p>{count} Items Total</p>
+      <Link href={`/products/${page + 1}`}>
+        <span aria-disabled={page >= pageCount}>Next</span>
+      </Link>
+    </PaginationStyles>
+  );
+};
+
+export default Pagination;
